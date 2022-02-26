2e6eec1a5a2425ccdab908ff54db8efc13d91e9f
Jesus Rivera <jgrcreations@gmail.com>
2022-02-25 19:13:08 -0500

    Updates

---
+++ main.9b2f3416.js
@@ -0,0 +1,7 @@
+import"./modulepreload-polyfill.6874861d.js";var e=[{key:"gato",title:"El Gato Con Botas",ruta:"pages/elGatoConBotas/"},{key:"mar",title:"El Viejo",ruta:"#"}];const o=document.querySelector(".colection-book");o.innerHTML=`
+  ${e.map(t=>`
+      <a class="book ${t.key}" href="${t.ruta}">
+        ${t.title}
+      </a>
+    `).join("")}
+`;
