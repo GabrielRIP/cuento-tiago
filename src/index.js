385a59aac5e9ff748b660095084bbb33a7dcc80d
Jesus Rivera <jgrcreations@gmail.com>
2022-02-25 19:10:07 -0500

    veremos

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const listBooks = document.querySelector('.colection-book')
 listBooks.innerHTML = `
   ${
     LIST_BOOKS.map(book => `
-      <a class="book ${book.key}" href="./pages/elGatoConBotas/">
+      <a class="book ${book.key}" href="${book.ruta}">
         ${book.title}
       </a>
     `)
