bd7cea01052c07993e283770e55c24e997917b87
Jesus Rivera <jgrcreations@gmail.com>
2022-02-28 16:17:56 -0500

    Updates

---
+++ pageGato.b31a5bd0.js
@@ -0,0 +1,22 @@
+import"./modulepreload-polyfill.6874861d.js";/* empty css              */var r=["Un molinero, al fallecer, dej\xF3 como herecia al mayor de sus tres hijos, un molino; al segundo, un burro; y al menor, llamado Tiago, un gato.","<< Con lo poco que he recibido morir\xE9 de hambre >>.","Se lamento el joven. De inmediato, el minino exclam\xF3 <<\xA1No se preocupe, yo lo convertir\xE9 en un pr\xEDncipe!...>>","- Solo deme un saco y unas botas que me protejan al caminar.","Tiago le entreg\xF3 al felino lo pedido y este parti\xF3 rumbo al bosque.","All\xED, caz\xF3 unos conejos y arregl\xF3 una entrevista con el rey de aquellas tierras:","Su majestad, el Marqu\xE9s de Carab\xE1s 't\xEDtulo que hab\xEDa inventado para su amo', le env\xEDa este obsequio.","El rey acogi\xF3 muy feliz al Gato con Botas; y los siguientes d\xEDas, rebibi\xF3 m\xE1s regalos a nombre de su due\xF1o.","Un d\xEDa, el minino le insisti\xF3 a su amo en ir a ba\xF1arse al r\xEDo, Tiago le tenia miedo al agua pero hacepto.","Al ver acercarse el carruaje real, el Gato con Botas grito '\xA1Socorro, el Marqu\xE9s de Carab\xE1s se ahora!'; y el rey de inmediato, orden\xF3 que lo auxiliaran.","Adem\xE1s, el sabido animal invent\xF3 que los hab\xEDan asaltado; por lo cual, el monarca orden\xF3 que vistiesen al 'Marqu\xE9s' con elegantes prendas.","El rey los invit\xF3 a su paseo, solo el Gato con Botas se adelant\xF3 hacia el castillo del Ogro con poderes m\xE1gicos.","Al llegar, lo reto <<\xA1A que no puedes convertirte en un le\xF3n!>>; el ogro vanidoso, lo hizo.","Luego el minino insisti\xF3 <<\xA1Pero no puedes volverte un rat\xF3n!>>, y el ogro tom\xF3 dicha forma.","Entonces, el felino se lo comi\xF3 y se apoder\xF3 de su inmenso castillo","Pronto, el Gato con Botas alcanz\xF3 al carruaje real y, con gran reverencia, los invit\xF3 a conocer el castillo del Marqu\xE9s de Carab\xE1s.","\xA1Todos estaban inpresionados!, sobre todo Tiago, quien adem\xE1s de t\xEDtulo, castillo y un leal servidor gatuno.","Termino enamorandose de la princesa real, con quien se cas\xF3 al poco tiempo.","...end..."],s="/cuento-tiago/image/gato-con-botas/img01.webp",t="/cuento-tiago/image/gato-con-botas/img02.webp",l="/cuento-tiago/image/gato-con-botas/img03.webp",c="/cuento-tiago/image/gato-con-botas/img04.webp",m="/cuento-tiago/image/gato-con-botas/img05.webp",u="/cuento-tiago/image/gato-con-botas/img06.webp";function e(n,i){return r.filter((a,o)=>o>=n&&o<=i).map(a=>`
+    <p>${a}</p>
+  `).join("")}document.querySelector(".container").innerHTML=`
+  <h1 class="title">El Gato Con Botas</h1>
+  <button class="return">
+    <a href="../../index.html">return</a>
+  </button>
+  <div class="box-content">
+    <img src="${s}" class="image-text">
+    ${e(0,3)}
+    <img src="${t}" class="image-text">
+    ${e(4,7)}
+    <img src="${l}" class="image-text">
+    ${e(8,10)}
+    <img src="${c}" class="image-text">
+    ${e(11,14)}
+    <img src="${m}" class="image-text">
+    ${e(15,17)}
+    <img src="${u}" class="image-text">
+    ${e(18,18)}
+  </div>
+`;
---
+++ pageTresCerditos.14a290c8.js
@@ -0,0 +1,43 @@
+import"./modulepreload-polyfill.6874861d.js";/* empty css              */var t=["En el bosque en el que viv\xEDan los tres cerditos hab\xEDa un gran revuelo. Al parecer, los p\xE1jaros hab\xEDan avisado a los ciervos de que un enorme lobo estaba a punto de llegar a sus tierras.","\u2013 \xA1Un lobo! \xA1Qu\xE9 miedo! Eso significa peligro, tendremos que pensar en c\xF3mo librarnos de \xE9l \u2013 exclam\xF3 el m\xE1s peque\xF1os de los tres cerditos.","Despu\xE9s de mucho pensar, los tres hermanos decidieron que lo mejor era construirse una casa donde poder estar a salvo de las garras del lobo. Sin embargo no se pon\xEDan de acuerdo en la manera de hacerla, as\xED que cada uno decidi\xF3 construir su propia casa.","El cerdito peque\xF1o decidi\xF3 hacer una casa con paja. Era mucho m\xE1s f\xE1cil que hacerla con otro material y as\xED no le costar\xEDa mucho esfuerzo.","El cerdito mediano prefiri\xF3 hacerla con madera. Era mucho m\xE1s resistente que la paja y como estaban en un bosque, la madera era f\xE1cil de conseguir. Adem\xE1s, tampoco le llevar\xEDa mucho tiempo ni esfuerzo.","El cerdito mayor pens\xF3 que lo mejor ser\xEDa hacerla con ladrillos. Es cierto que aquello le llevar\xEDa mucho tiempo y esfuerzo, pero le pareci\xF3 que solo si la casa era de ladrillos, podr\xEDa protegerle del malvado lobo.","El cerdito peque\xF1o y el cerdito mediano hac\xEDa mucho que hab\xEDan terminado sus casas, y el cerdito mayor, segu\xEDa con su gran obra.","\u2013 Como no te des prisa \u2013 le dec\xEDan \u2013 llegar\xE1 el lobo y no habr\xE1 servido de nada tanto esfuerzo, ya que tu casa no estar\xE1 terminada y no te quedar\xE1 m\xE1s remedio que venirte a la nuestra.","Pero el cerdito mayor no les hac\xEDa caso. Sab\xEDa lo importante que era el trabajo bien hecho y sin prisa, pero sin pausa, fue terminando su casa de ladrillos. Justo a tiempo.","Y es que el lobo lleg\xF3 precisamente el d\xEDa de su inauguraci\xF3n. Cuando el rumor de que el malvado malvad\xEDsimo lobo hab\xEDa llegado al bosque, cada cerdito se escondi\xF3 en su casa. \xA1Qu\xE9 miedo!","Para colmo de males, aquella tarde se hab\xEDa levantado una fuerte tormenta. \xA1Con lo poco que le gustaban a los cerditos las tormentas! Muy asustado, el cerdito peque\xF1o se asom\xF3 por la ventana de su caja de paja.","\u2013 \xA1Ay qu\xE9 ver este viento! Est\xE1 tambaleando tanto mi casa que parece como si la fuera a tirar.","Pero al mirar por la ventana, lo que vio el cerdito peque\xF1o fue al malvado malvad\xEDsimo lobo. \xA1Ten\xEDa unos colmillos tan grandes!","\u2013 \xA1No es el viento lo que est\xE1 tambaleando la casa! Es el lobo que est\xE1 soplando\u2026","Y antes de que se diera cuenta, la casa de paja se hab\xEDa desvanecido. El peque\xF1o cerdito corri\xF3 y corri\xF3 hasta la casa de su hermano mediano.","\u2013 Aqu\xED estaremos a salvo \u2013 le protegi\xF3 el cerdito de la casa de madera.","Pero afuera, la tormenta se hab\xEDa vuelto m\xE1s y m\xE1s dura. Llov\xEDa a c\xE1ntaros, mojando la madera de la casa del cerdito mediano. Adem\xE1s aquel viento tan molesto\u2026\xA1y el lobo, que otra vez estaba plantado frente a la casa de los cerditos!","\u2013 \xA1Ya est\xE1 aqu\xED otra vez! Empezar\xE1 a soplar y a soplar\u2026\xA1y derribar\xE1 la casa!","Y antes de que hubieran terminado de decirlo, la casa de madera se hab\xEDa desplomado. Los dos cerditos corrieron y corrieron hasta la casa de ladrillo del hermano mayor.","\u2013 Aqu\xED estaremos a salvo \u2013 les protegi\xF3 el cerdito mayor.","Y para su sorpresa, los cerditos peque\xF1os descubrieron que ni la tormenta, ni el viento, ni el lobo malvado malvad\xEDsimo, pod\xEDan destruir aquella casa tan bien hecha.","\u2013 \xA1Os lo dije! Las cosas bien hechas necesitan m\xE1s esfuerzo, pero luego duran para siempre\u2026","Estaban tan contentos los tres cerditos en la casa de ladrillo, que casi se hab\xEDan olvidado del lobo y de la tormenta cuando un ruido les sobresalt\xF3. Era el timbre, \xBFqui\xE9n llamar\xEDa a esas horas en una tarde tan desapacible?","\u2013 \xA1Es el lobo! \u2013 exclam\xF3 asustado el hermano mayor cuando mir\xF3 por la mirilla de la puerta.","\u2013 S\xED, soy el lobo \u2013 exclam\xF3 el animal que hab\xEDa escuchado lo que el cerdito hab\xEDa dicho.","\u2013 Pues fuera de aqu\xED, ya has destruido dos casas, pero esta no conseguir\xE1s tirarla.","El lobo suspir\xF3 con tristeza y exclam\xF3:","\u2013 \xBFLa casa de paja y la casa de madera? Yo no tuve nada que ver con eso. Estaban tan mal construidas que la propia tormenta acab\xF3 con ellas.","\u2013 Y entonces, \xBFqu\xE9 haces aqu\xED?","\u2013 Soy nuevo en el bosque, y he venido a invitar a todos los animales a una gran fiesta. As\xED podremos conocernos\u2026","\u2013 Querr\xE1s decir que podr\xE1s comernos\u2026","El lobo volvi\xF3 a suspirar con tristeza y grit\xF3:","\u2013 \xBFPor qu\xE9 dec\xEDs eso? No sab\xE9is nada de m\xED y sin embargo ya dais por hecho que soy un lobo malo.","\u2013 Es que todos los lobos son malos y quieren comernos\u2026","\u2013 Pero yo no, \xA1si soy un lobo vegetariano!","Los tres cerditos se miraron con miedo. \xBFPod\xEDan confiar en aquel lobo? Para comprobar que era verdad lo que dec\xEDa, le pusieron una prueba.","\u2013 Si es verdad que eres vegetariano, tendr\xE1s que demostrarlo.","Y por debajo de la puerta, los tres cerditos le pasaron una bandeja con comida. En un plato hab\xEDa un suculento trozo de carne. En el otro una ensalada bien fresca.","El lobo no dud\xF3 ni un instante, cogi\xF3 el tenedor y comenz\xF3 a comerse la ensalada.","\u2013 Necesitar\xEDa un poco de aceite y vinagre\u2026\xA1esta ensalada est\xE1 sin ali\xF1ar!","Los tres cerditos comprendieron que aquel lobo no ment\xEDa y confiaron en \xE9l. Y as\xED fue como aquel lobo vegetariano se qued\xF3 para siempre en el bosque, y \xE9l y los tres cerditos (que terminaron viviendo todos juntos en la casa de ladrillos) fueron amigos para siempre.","...end..."],l="/cuento-tiago/image/los-tres-cerditos/image01.webp",i="/cuento-tiago/image/los-tres-cerditos/image02.webp",n="/cuento-tiago/image/los-tres-cerditos/image03.webp",c="/cuento-tiago/image/los-tres-cerditos/image04.webp",d="/cuento-tiago/image/los-tres-cerditos/image05.webp",u="/cuento-tiago/image/los-tres-cerditos/image06.webp",m="/cuento-tiago/image/los-tres-cerditos/image07.webp",g="/cuento-tiago/image/los-tres-cerditos/image08.webp",p="/cuento-tiago/image/los-tres-cerditos/image09.webp",b="/cuento-tiago/image/los-tres-cerditos/image10.webp",q="/cuento-tiago/image/los-tres-cerditos/image11.webp",v="/cuento-tiago/image/los-tres-cerditos/image12.webp",h="/cuento-tiago/image/los-tres-cerditos/image13.webp",y="/cuento-tiago/image/los-tres-cerditos/image14.webp",$="/cuento-tiago/image/los-tres-cerditos/image15.webp";function e(s,r){return t.filter((a,o)=>o>=s&&o<=r).map(a=>`
+    <p>${a}</p>
+  `).join("")}document.querySelector(".container").innerHTML=`
+  <h1 class="title">Los tres Cerditos</h1>
+  <button class="return">
+    <a href="../../index.html">return</a>
+  </button>
+  <div class="box-content">
+    <p class="text-prologo">
+      <strong style="color: tomato">Los tres cerditos</strong> eran tres hermanos que deciden construirse una casita cada uno a su manera para protegerse del lobo feroz. Seguro que todos conoc\xE9is esa versi\xF3n del <strong style="color: tomato">cuento de los tres cerditos y el lobo feroz</strong> que quer\xEDa com\xE9rselos. Pero \xBFy si hubiera otro final para el cuento de <strong style="color: tomato">los 3 cerditos</strong> que no te imaginas\u2026? A veces las apariencias enga\xF1an y no todo es como parece.
+    </p>
+    <img src="${l}" class="image-text">
+    ${e(0,2)}
+    <img src="${i}" class="image-text">
+    ${e(3,3)}
+    <img src="${n}" class="image-text">
+    ${e(4,4)}
+    <img src="${c}" class="image-text">
+    ${e(5,5)}
+    <img src="${d}" class="image-text">
+    ${e(6,8)}
+    <img src="${u}" class="image-text">
+    ${e(9,10)}
+    <img src="${m}" class="image-text">
+    ${e(11,12)}
+    <img src="${g}" class="image-text">
+    ${e(13,14)}
+    <img src="${p}" class="image-text">
+    ${e(15,17)}
+    <img src="${b}" class="image-text">
+    ${e(18,21)}
+    <img src="${q}" class="image-text">
+    ${e(22,25)}
+    <img src="${v}" class="image-text">
+    ${e(26,31)}
+    <img src="${h}" class="image-text">
+    ${e(32,37)}
+    <img src="${y}" class="image-text">
+    ${e(38,40)}
+    <img src="${$}" class="image-text">
+    ${e(41,41)}
+  </div>
+`;
